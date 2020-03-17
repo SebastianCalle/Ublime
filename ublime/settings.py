393fdbf07366d0455c780770f655a2e965ef3f1b
Juan Sebastian Calle <jusemonca@gmail.com>
2020-03-17 18:57:37 -0500

    [fix]: Change django-heroku

--- ublime/settings.py
+++ ublime/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 import django_heroku
 
-django_heroku.settings(locals())
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 BASE_TEMPLATES = os.path.join(BASE_DIR, 'templates')
@@ -143,3 +142,4 @@ LEAFLET_CONFIG = {
     'MIN_ZOOM': 10,
     'MAX_ZOMM': 18,
 }
+django_heroku.settings(locals())
