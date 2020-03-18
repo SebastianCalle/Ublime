fa2e82ee2e1020835328be1691980bcfea84a013
Juan Sebastian Calle <jusemonca@gmail.com>
2020-03-18 10:38:47 -0500

    [Feature]: Add GEOS AND GDAL library

--- ublime/settings.py
+++ ublime/settings.py
@@ -144,6 +144,6 @@ LEAFLET_CONFIG = {
     'MIN_ZOOM': 10,
     'MAX_ZOMM': 18,
 }
-GEOS_LIBRARY_PATH = environ.get('GEOS_LIBRARY_PATH')
-GDAL_LIBRARY_PATH = environ.get('GDAL_LIBRARY_PATH')
+GEOS_LIBRARY_PATH = os.environ.get('GEOS_LIBRARY_PATH')
+GDAL_LIBRARY_PATH = os.environ.get('GDAL_LIBRARY_PATH')
 django_heroku.settings(locals())
