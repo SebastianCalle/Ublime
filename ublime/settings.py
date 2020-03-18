7c3ac08564fae1b8c4093d520850cecf8e8e54a4
Juan Sebastian Calle <jusemonca@gmail.com>
2020-03-18 12:13:49 -0500

    [Feature]: test deploy without django_heroku

--- ublime/settings.py
+++ ublime/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -146,4 +145,3 @@ LEAFLET_CONFIG = {
 }
 GEOS_LIBRARY_PATH = os.environ.get('GEOS_LIBRARY_PATH')
 GDAL_LIBRARY_PATH = os.environ.get('GDAL_LIBRARY_PATH')
-django_heroku.settings(locals())
