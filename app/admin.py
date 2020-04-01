b53e76de092b97f3dc5e0ed53ee72498b7fd4eac
SebastianCalle <jusemonca@gmail.com>
2020-03-31 22:42:08 -0500

    [Feature]: delete leaflet

--- app/admin.py
+++ app/admin.py
@@ -1,9 +1,4 @@
 from django.contrib import admin
-from leaflet.admin import LeafletGeoAdmin
-
 from .models import Toilet
-class ToiletAdmin(LeafletGeoAdmin):
-    # load the map for admin site
-    pass
 
-admin.site.register(Toilet, ToiletAdmin)
+admin.site.register(Toilet)
