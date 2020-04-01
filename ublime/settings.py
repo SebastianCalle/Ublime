c608fa896e576f91cd790254045ab078099a3e52
SebastianCalle <jusemonca@gmail.com>
2020-03-31 23:20:49 -0500

    [Feature]: delete leaflet

--- ublime/settings.py
+++ ublime/settings.py
@@ -25,7 +25,7 @@ BASE_TEMPLATES = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = 'yic)wf*ab_d0n0&#rj*6thke1!sejk$%!j7ag8qnjg&eimk5*#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['ublime.herokuapp.com', '127.0.0.1']
 
